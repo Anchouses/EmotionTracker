82befe4c649542eeca77c52dc4761fc49152447d
Anchous <foranngel@gmail.com>
2023-08-07 21:33:52 +0300

    ann-3 наброски первых 2х экранов

--- app/src/main/java/com/emotiontracker/MainActivity.kt
+++ app/src/main/java/com/emotiontracker/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val noteFragment = supportFragmentManager.findFragmentById(R.id.note_fragment)
 
     }
 
